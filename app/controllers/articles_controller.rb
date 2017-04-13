87f5e389ee5f9f674baeba0fb1397b5a0703bb7e
rails udemy course <railsudemycoursegmail.com>
2017-04-13 19:20:58 +0000

    all actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,61 @@
 class ArticlesController < ApplicationController
-    
+before_action :set_article,only: [:edit,:update,:show,:destroy]
+    def index
+        @articles=Article.all
+    end    
     def new
         @article=Article.new
         
     end
+    
+    def edit
+
+    end
 
 def create
-    #render plain: params[:article,:article].inspect
+    
     @article=Article.new(article_params)
-    @article.save
-    redirect_to articles_shows(@article)
+ 
+ if @article.save
+
+    flash[:notice]="article is sucessfully created" 
+    redirect_to article_path(@article)
+    
+    
+else
+    render 'new'
+end
+end
+def show
+
+   @article=Article.find(params[:id])
+
+    
+end
+def update
+    if @article.update(article_params)
+
+    flash[:notice]="article is sucessfully updated" 
+    redirect_to article_path(@article)
+    
+    else
+    render 'edit'
 end
 
+    
+end    
+def destroy
+    @article.destroy
+    flash[:notice]="article was sucessfully deleted"
+    redirect_to articles_path
+    
+end    
+    
+
 private 
+def set_article
+    @article=Article.find(params[:id])
+end    
 def article_params
     params.require(:article).permit(:title,:description)
 
